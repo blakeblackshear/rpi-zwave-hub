a3b7aceb4415ed119f3e28df713e016acff69828
blakeblackshear <blakeb@blakeshome.com>
2018-11-04 06:33:19 -0600

    updates for cooper_controllers component

--- homeassistant/custom_components/cooper_controllers.py
+++ homeassistant/custom_components/cooper_controllers.py
@@ -7,7 +7,10 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers import discovery
 from homeassistant.components.zwave.const import DOMAIN as ZWAVE_DOMAIN
-from homeassistant.components.zwave.const import EVENT_SCENE_ACTIVATED, DATA_DEVICES, EVENT_NETWORK_COMPLETE
+from homeassistant.components.zwave.const import (
+    EVENT_SCENE_ACTIVATED, DATA_DEVICES, EVENT_NETWORK_COMPLETE, EVENT_NODE_EVENT, 
+    SERVICE_REFRESH_NODE_VALUE, DATA_NETWORK, COMMAND_CLASS_INDICATOR, EVENT_NETWORK_READY,
+    EVENT_NETWORK_COMPLETE_SOME_DEAD)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -15,33 +18,69 @@ DEPENDENCIES = ['zwave']
 DOMAIN = 'cooper_controllers'
 CONF_CONTROLLERS = 'controllers'
 
-CONTROLLER_SCHEMA = {
-    vol.Required('node_id'): vol.Coerce(int),
-    vol.Required('indicator'): cv.entity_id
-}
-
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Required(CONF_CONTROLLERS): vol.All(cv.ensure_list, 
-            [CONTROLLER_SCHEMA])
-    })
-}, extra=vol.ALLOW_EXTRA)
-
 def setup(hass, config):
     """Set up the Cooper Switch switch platform."""
-    cooper_config = config[DOMAIN]
-
     controllers = {}
-    for controller in cooper_config['controllers']:
-        controllers[controller['node_id']] = controller['indicator']
 
-    _LOGGER.info("Cooper config %s",str(controllers))
+    def _discover_cooper_switches(states):
+        for state in states:
+            if (state.domain == 'zwave' and 
+            'product_name' in state.attributes and
+            state.attributes['product_name'] == 'RFWC5/RFWDC Scene Controller'):
+                controllers[state.attributes['node_id']] = {}
+
+    def _populate_indicators(states):
+        network = hass.data.get(DATA_NETWORK)
+        for node_id in controllers:
+            node = network.nodes.get(node_id)
+            if node is None:
+                return
+            
+            # fetch the value id for the indicator
+            indicator_value_id = None
+            if node.has_command_class(COMMAND_CLASS_INDICATOR):
+                for value_id in node.get_values(
+                        class_id=COMMAND_CLASS_INDICATOR):
+                    indicator_value_id = value_id
+                    break
+
+            # find the indicator sensor entity id
+            for state in states:
+                if (state.domain == 'sensor' and 
+                'value_id' in state.attributes and
+                state.attributes['value_id'] == str(indicator_value_id)):
+                    controllers[node_id]['indicator'] = state.entity_id
+
+    def _populate_associations(states):
+        # build a map of node_id to entity_id for lights and switches
+        zwave_nodes = {}
+        for state in states:
+            if 'node_id' not in state.attributes:
+                continue
+            if state.domain not in ['switch', 'light']:
+                continue
+            node_id = state.attributes['node_id']
+            zwave_nodes[node_id] = state.entity_id
+
+        # lookup associations and map node_ids to entity ids
+        network = hass.data.get(DATA_NETWORK)
+        for node_id in controllers:
+            node = network.nodes.get(node_id)
+            if node is None:
+                return
+            groupdata = node.groups
+            groups = {}
+            for key, value in groupdata.items():
+                associated_entities = [zwave_nodes[association] for association in value.associations
+                                        if association in zwave_nodes]
+                groups[key] = {'associations': associated_entities}
+            controllers[node_id]['groups'] = groups
 
     def _update_cooper_indicator_state(event):
         node_id=event.data['node_id']
         scene_id=event.data['scene_id']
         if node_id in controllers:
-            entity_id = controllers[node_id]
+            entity_id = controllers[node_id]['indicator']
             indicator_state = hass.states.get(entity_id)
             indicator_value = int(indicator_state.state)
             if scene_id==1 and (indicator_value & 1)==0:
@@ -56,15 +95,35 @@ def setup(hass, config):
                 indicator_value = indicator_value+16
             hass.states.set(entity_id, indicator_value, indicator_state.attributes)
 
-        # discovery.load_platform(
-        #     hass, 'cooper_controllers', 'switch', discovered={},
-        #     hass_config=config)
-    # wait for zwave ready
+    def _refresh_cooper_indicator_state(event):
+        node_id=event.data['node_id']
 
-    def _find_cooper_controllers(event):
-        _LOGGER.info("%s",str(hass.data['entity_registry']))
+        if node_id in controllers:
+            entity_id = controllers[node_id]['indicator']
+            indicator_state = hass.states.get(entity_id)
+
+            data = {
+                "node_id": node_id,
+                "value_id": indicator_state.attributes['value_id']
+            }
+            hass.async_add_job(hass.services.async_call('zwave', SERVICE_REFRESH_NODE_VALUE, data))
+
+    def _create_cooper_switches(event):
+        if len(controllers)>0:
+            return
+        states = hass.states.all()
+        _discover_cooper_switches(states)
+        _populate_indicators(states)
+        _populate_associations(states)
+        _LOGGER.info("Discovered Cooper Controllers %s",str(controllers))
+        hass.data[DOMAIN] = controllers
+        discovery.load_platform(
+            hass, 'switch', 'cooper_controllers', discovered={},
+            hass_config=config)
 
     hass.bus.listen(EVENT_SCENE_ACTIVATED, _update_cooper_indicator_state)
-
-    #hass.bus.listen_once(EVENT_NETWORK_COMPLETE, _find_cooper_controllers)
+    hass.bus.listen(EVENT_NODE_EVENT, _refresh_cooper_indicator_state)
+    hass.bus.listen_once(EVENT_NETWORK_READY, _create_cooper_switches)
+    hass.bus.listen_once(EVENT_NETWORK_COMPLETE, _create_cooper_switches)
+    hass.bus.listen_once(EVENT_NETWORK_COMPLETE_SOME_DEAD, _create_cooper_switches)
     return True
---
+++ homeassistant/custom_components/switch/cooper_controllers.py
@@ -0,0 +1,129 @@
+"""Switches for Cooper Controllers."""
+import datetime
+import logging
+
+from homeassistant.components.switch import SwitchDevice
+from homeassistant.components.zwave.const import SERVICE_SET_NODE_VALUE, SERVICE_REFRESH_NODE_VALUE
+from homeassistant.helpers.event import async_track_state_change, async_call_later
+
+DEPENDENCIES = ['cooper_controllers']
+DOMAIN = 'cooper_controllers'
+
+_LOGGER = logging.getLogger(__name__)
+
+BUTTON_VALUES = {
+    1: 1,
+    2: 2,
+    3: 4,
+    4: 8,
+    5: 16
+}
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Cooper Controller switch platform."""
+    switches = []
+
+    controllers = hass.data[DOMAIN]
+
+    for node_id in controllers:
+        for button in [1,2,3,4,5]:
+            switches.append(CooperControllerSwitch(node_id, controllers[node_id], button))
+
+    add_entities(switches)
+
+
+class CooperControllerSwitch(SwitchDevice):
+    """A Cooper Controller button."""
+
+    def __init__(self, node_id, controller, button):
+        """Initialize a button for a Cooper Controller."""
+        self._node_id = node_id
+        self._controller = controller
+        self._button = button
+        self._state = False
+        self._indicator_entity_id = controller['indicator']
+        self._associations = self._controller['groups'][self._button]['associations']
+    
+    async def async_added_to_hass(self):
+        """Subscribe to state changes for indicator events."""
+        def _refresh_associated(*_):
+            for association in self._associations:
+                entity = self.hass.states.get(association)
+                data = {
+                    "node_id": entity.attributes['node_id'],
+                    "value_id": entity.attributes['value_id']
+                }
+                self.hass.async_add_job(self.hass.services.async_call('zwave', SERVICE_REFRESH_NODE_VALUE, data))
+
+        def _state_updated(entity_id, old_state, new_state):
+            """Schedule an update."""
+            async_call_later(self.hass, 4, _refresh_associated)
+            self.async_schedule_update_ha_state()
+
+        def _sync_associated(entity_id, old_state, new_state):
+            all_off = True
+            for association in self._associations:
+                if self.hass.states.get(association).state == 'on':
+                    all_off = False
+                    break
+            
+            if all_off and self.is_on:
+                self.turn_off()
+            elif not all_off and not self.is_on:
+                self.turn_on()
+
+        async_track_state_change(
+            self.hass, self._indicator_entity_id, _state_updated
+        )
+
+        for associated_entity in self._associations:
+            async_track_state_change(
+                self.hass, associated_entity, _sync_associated
+            )
+    
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return False
+
+    @property
+    def name(self):
+        """Return the name of the switch."""
+        return "Node {0} Button {1}".format(self._node_id, self._button)
+
+    @property
+    def is_on(self):
+        """Get the state of the button based on the indicator value."""
+        indicator_state = self.hass.states.get(self._indicator_entity_id)
+        indicator_value = int(indicator_state.state)
+        return indicator_value & BUTTON_VALUES[self._button] != 0
+
+    def turn_on(self, **kwargs):
+        """Turn on the led for the button."""
+        indicator_state = self.hass.states.get(self._indicator_entity_id)
+        indicator_value = int(indicator_state.state)
+
+        if indicator_value & BUTTON_VALUES[self._button] != 0:
+            return
+
+        data = {
+            "node_id": self._node_id,
+            "value_id": indicator_state.attributes['value_id'],
+            "value": indicator_value + BUTTON_VALUES[self._button]
+        }
+        self.hass.async_add_job(self.hass.services.async_call('zwave', SERVICE_SET_NODE_VALUE, data))
+
+    def turn_off(self, **kwargs):
+        """Turn on the led for the button."""
+        indicator_state = self.hass.states.get(self._indicator_entity_id)
+        indicator_value = int(indicator_state.state)
+
+        if indicator_value & BUTTON_VALUES[self._button] == 0:
+            return
+
+        data = {
+            "node_id": self._node_id,
+            "value_id": indicator_state.attributes['value_id'],
+            "value": indicator_value - BUTTON_VALUES[self._button]
+        }
+        self.hass.async_add_job(self.hass.services.async_call('zwave', SERVICE_SET_NODE_VALUE, data))
